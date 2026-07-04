43690c7944d50b5e97d956422f1834bae34ccf32
safiurahmanjami <webcoding44@gmail.com>
2026-07-04 14:28:09 +0430

    add export const runtime = "nodejs";

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
-
+export const runtime = "nodejs";
 const authHandlers = toNextJsHandler(auth.handler);
 
 export const { GET, POST } = authHandlers;
