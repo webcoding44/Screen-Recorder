201c4bc2f987b11bda818ef9522b4701ffd9f5c8
safiurahmanjami <webcoding44@gmail.com>
2026-07-05 09:53:02 +0430

    update version next.js

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 
 export async function middleware(request: NextRequest) {
   const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+  headers: request.headers,
+});
+  // const session = await auth.api.getSession({
+  //   headers: await headers(),
+  // });
 
   if (!session) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
